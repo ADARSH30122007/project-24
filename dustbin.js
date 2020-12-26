01bcd2bb535b1524ee60d22ce640b893e96d230f
ADARSH30122007 <74131191+ADARSH30122007@users.noreply.github.com>
2020-12-26 19:31:21 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,13 @@
+
+	rectangle1=Bodies.rectangle(width/2, 650,100,20 , {isStatic:true} );
+	rectangle1.scale=0.3;
+	rectangle1.shapeColor=("red");
+
+	rectangle2=Bodies.rectangle(width/2, 650,100,20 , {isStatic:true} );
+	rectangle2.scale=0.3;
+	rectangle2.shapeColor=("red");
+
+
+	rectangle3=Bodies.rectangle(width/2, 650,100,20, {isStatic:true} );
+	rectangle3.scale=0.3;
+    rectangle3.shapeColor=("red");
